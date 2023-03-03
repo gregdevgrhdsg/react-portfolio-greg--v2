20ce9b79607718d7df83a08599cc807c2f1b48ae
Grégoire Prunet-Foch <gprunetf@gpf.home>
2023-03-03 14:56:14 +0100

    pdf download

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ai from '../../assets/logoHero.svg';
+import pdf from '../../assets/my_CV.pdf';
 import './header.css';
 
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-    <h2 className='hello__header_name'>
-          Hi 👋🏻 I'm Greg
-        </h2>
+      <h2 className='hello__header_name'>
+        Hi 👋🏻 I'm Greg
+      </h2>
       <h1 className="gradient__text">I'm a graphic web designer</h1>
       <p> Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
       <div className="gpt3__header-content__input">
-      <button href=".https://hammerhead-app-oxaqg.ondigitalocean.app/assets/my_CV.pdf" download class="button color-1">Télécharger mon CV</button>
+       <a href={pdf} download><button className="button color-1">Télécharger mon CV</button></a>
       </div>
     </div>
 
