fdcc73947900423fa6730a27e161f70440e1f334
Grégoire Prunet-Foch <gprunetf@gpf.home>
2023-03-03 16:09:03 +0100

    version header final

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => (
         Hi 👋🏻 I'm Greg
       </h2>
       <h1 className="gradient__text">I'm a graphic web designer</h1>
-      <p> Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <p> As a graphic designer, I strive to bring creativity and innovation to every project. Design is not just about aesthetics, it's about solving problems and communicating ideas visually. </p>
 
       <div className="gpt3__header-content__input">
        <a href={pdf} download><button className="button color-1">Télécharger mon CV</button></a>
