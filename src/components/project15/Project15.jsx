9d14d5982878db4c11795a59b9c6f2b2446f1b82
Grégoire Prunet-Foch <gprunetf@macbook-pro.home>
2023-09-29 10:22:00 +0200

    new button website beach house

--- src/components/project15/Project15.jsx
+++ src/components/project15/Project15.jsx
@@ -10,6 +10,7 @@ const Project15 = () => {
           <Link to="/portfolio" className="back-button">⬅️ Back</Link>
       </div>
       <h2 className="section__title">Beach House</h2>
+      <div className="section__title"><a href="https://www.beachhouserum.com" download><button className="button color-1">Visiter le site</button></a></div>
       <div className='skills__grid__prj'>
              <img className='skills__img__prj' src={Prj17} alt="HTML icon" />
         </div>
