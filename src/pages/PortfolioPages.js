1d35361c69672b0016de682dcdb33aec7ed156b0
Grégoire Prunet-Foch <gprunetf@macbook-pro.home>
2023-10-04 15:34:59 +0200

    scrollfix portfolio

--- src/pages/PortfolioPages.js
+++ src/pages/PortfolioPages.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import Navbar from '../components/navbar/Navbar'
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Navbar from '../components/navbar/Navbar';
 import { Footer } from '../containers';
-import Portfolio from '../components/portfolio/Portfolio'
+import Portfolio from '../components/portfolio/Portfolio';
 import './portfolioPages.css';
 
 function PortfolioPages() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/portfolio') {
+      // Défilement vers le haut uniquement pour la page de portefeuille
+      window.scrollTo(0, 0);
+    }
+  }, [location]);
+
   return (
     <div className='App'>
-    <div className='gradient__bg'>
-      <Navbar />
-    </div>
+      <div className='gradient__bg'>
+        <Navbar />
+      </div>
       <div className='portfolio__container'>
-      <Portfolio />
-      <Footer />
+        <Portfolio />
+        <Footer />
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default PortfolioPages
+export default PortfolioPages;
